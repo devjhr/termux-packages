e0c0073769ec11b8c67d0c115d3613ea28a5ac24
MD Jahangir Alam <labibpop120@gmail.com>
2026-02-23 00:11:08 +0600

    Update generate-bootstraps.sh

--- scripts/generate-bootstraps.sh
+++ scripts/generate-bootstraps.sh
@@ -23,7 +23,7 @@ TERMUX_PACKAGE_MANAGERS=("apt" "pacman")
 
 # The repository base urls mapping for package managers.
 declare -A REPO_BASE_URLS=(
-	["apt"]="https://packages-cf.termux.dev/apt/termux-main"
+	["apt"]="https://devjhr.github.io/pkg-repo"
 	["pacman"]="https://s3.amazonaws.com/termux-pacman.us/main"
 )
 
